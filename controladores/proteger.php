b7deb45453a4665bcbc9002f387c3b1aac08c49c
Mikel <bnm12534@gmail.com>
2026-04-19 19:50:28 +0200

    correcion de proteger

--- controladores/proteger.php
+++ controladores/proteger.php
@@ -1,8 +1,10 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 if (!isset($_SESSION['usuario_id'])) {
-    header("Location: ../vista/loginVista.php?server_msg=" . urlencode("Debes iniciar sesión para acceder a esta página.") . "&creacion=error");
+    header("Location: ../VISTA/loginVista.php");
     exit();
 }
 ?>
